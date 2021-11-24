fd18bfd071882f0aed9f631980c18ff7668e1a38
Francis <francisfranky24@gmail.com>
2021-11-24 12:51:27 +0530

    js

---
+++ js prc/prc.js
@@ -0,0 +1,58 @@
+console.clear()
+var num1=0
+var num2=0
+var div=num1/num2
+console.log(0/0)
+var num1=0
+var num2=5
+var div=num1/num2
+console.log(0/5)
+console.log(typeof(div))
+var num1=3
+var num2='a'
+var mul=num1*num2
+console.log(mul)
+var num1='a'
+var num2=3
+var mul=num1*num2
+console.log(mul)
+var num1='b'
+var num2=4
+var sum=num1+num2
+console.log(sum)
+console.log(typeof(sum))
+var num1=4
+var num2='b'
+var sum=num1+num2
+console.log(sum)
+console.log(typeof(sum))
+var num1=5
+var num2='c'
+var minus=num1-num2
+console.log(minus)
+var num1='c'
+var num2=5
+var minus=num1-num2
+console.log(minus)
+var num=100
+console.log(num)
+console.log(typeof(num))
+console.log(typeof(num.toString()))
+var num='44'
+console.log(parseInt(num))
+var num='22.222'
+console.log(parseInt(num))
+var num='Banana'
+console.log(parseInt(num))
+var num=12
+console.log(parseFloat(num))
+var num=13.3
+console.log(parseFloat(num))
+var num=10
+console.log(num.toFixed(2))
+var num=15.456789
+console.log(num.toFixed())
+var num=15.456789
+console.log(num.toFixed(2))
+var num=15.456789
+console.log(num.toFixed(3))
